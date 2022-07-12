9efd119c494a1dc969635e7296def1855780f88b
SatyamguptaSG <105717874+SatyamguptaSG@users.noreply.github.com>
2022-07-12 21:31:27 +0530

    Update hashfunc.v

--- hashfunc.v
+++ hashfunc.v
@@ -1,11 +1,14 @@
 `timescale 1ns/1ns
-module hashfunc(input key,input clk, output reg hash1, output reg hash2);
+module hashfunc(input [31:0]key,input clk, output reg [31:0] hash1, output reg [31:0] hash2);
 
-always @(posedge clk) begin
-hash1=(((key*9)%100)*11)%100;
- hash2=(((key*45)%1000)*22)%100;
+reg [31:0] key_in;
+
+always @(key) begin
+key_in=key;
+hash1=((((key_in*91)%100)*11)-(((key_in*91)%100)*11)%100)/100;
+hash2=((((key_in*45)%100)*22)-(((key_in*45)%100)*22)%100)/100;
+
+$display("hash1: %d  hash2: %d ",hash1, hash2 );
 end
 endmodule
-
-
 
